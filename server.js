7084ed6a9132bf894f57a3a8203115fcb343f5a9
Hal DeLage <hal.delage@teamaol.com>
2017-01-21 12:50:01 -0500

    Configured for Heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname));
+
+//handle request to api
+app.get('/api/posts', (req, res) => {
+  res.send([{ id: 1 }]);
+});
+
+//handle request to react app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen(port);
+console.log('Server started on port', port);
